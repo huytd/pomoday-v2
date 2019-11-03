3a3269d6876ef63635237719fff2c55bd7b136bb
Evan Nguyen <dacsang97@gmail.com>
2019-11-03 18:15:18 +0700

    Reduce CSS size by purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,36 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.html',
+    './src/**/*.tsx',
+    // etc.
+  ],
+  whitelist: [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'ul',
+    'ol',
+    'li',
+    'p',
+    'pre',
+    'blockquote',
+    'figure',
+    'hr',
+    'table',
+    'img',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
   plugins: [
-    require('tailwindcss')('./tailwind.config.js')
-  ]
-}
+    require('tailwindcss')('./tailwind.config.js'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+};
