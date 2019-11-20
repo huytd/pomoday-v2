54684e93a2cce701b97ad8daa42c7d0462fbae6e
Huy Tran <kingbazoka@gmail.com>
2019-11-19 18:22:50 -0800

    Add some missing test for command parser

--- test/helpers/commands.test.ts
+++ test/helpers/commands.test.ts
@@ -2,6 +2,7 @@ import * as test from 'tape';
 import { Command, parseCommand } from '../../src/helpers/commands/parser';
 
 const tests: [string, Command][] = [
+  // new task
   [
     't @work This is a new task',
     { command: 't', tag: '@work', text: 'This is a new task' },
@@ -10,12 +11,19 @@ const tests: [string, Command][] = [
     'task @longer-task This is another task',
     { command: 'task', tag: '@longer-task', text: 'This is another task' },
   ],
+  // start task timer
   ['b 10', { command: 'b', id: '10' }],
   ['begin 12', { command: 'begin', id: '12' }],
+  // check task as done
   ['c 7', { command: 'c', id: '7' }],
   ['check 9', { command: 'check', id: '9' }],
+  // delete a task
   ['d 1', { command: 'd', id: '1' }],
   ['delete 3', { command: 'delete', id: '3' }],
+  ['delete 3 5 79', { command: 'delete', id: '3 5 79' }],
+  ['delete @tag', { command: 'delete', id: '@tag' }],
+  ['delete done', { command: 'delete', id: 'done' }],
+  // edit a task
   [
     'e 1 this is a new task description',
     { command: 'e', id: '1', text: 'this is a new task description' },
@@ -24,20 +32,32 @@ const tests: [string, Command][] = [
     'edit 1 a new task description goes here',
     { command: 'edit', id: '1', text: 'a new task description goes here' },
   ],
+  // archive a task
+  ['a 1', { command: 'a', id: '1' }],
+  ['archive 3', { command: 'archive', id: '3' }],
+  ['archive 3 5 79', { command: 'archive', id: '3 5 79' }],
+  ['archive @tag', { command: 'archive', id: '@tag' }],
+  ['archive done', { command: 'archive', id: 'done' }],
+  // move task to new tag
   ['mv 2 @new-tag', { command: 'mv', id: '2', tag: '@new-tag' }],
   [
     'move 3 @uncategorized',
     { command: 'move', id: '3', tag: '@uncategorized' },
   ],
+  // flag a task
   ['fl 2', { command: 'fl', id: '2' }],
   ['flag 2', { command: 'flag', id: '2' }],
+  // stop timer
   ['st 1', { command: 'st', id: '1' }],
   ['stop 1', { command: 'stop', id: '1' }],
+  // other command
   ['help', { command: 'help' }],
   ['close-help', { command: 'close-help' }],
   ['today', { command: 'today' }],
   ['dark', { command: 'dark' }],
   ['light', { command: 'light' }],
+  ['customize', { command: 'customize' }],
+  ['list-archived', { command: 'list-archived' }],
 ];
 
 tests.forEach(([input, expected]) => {
