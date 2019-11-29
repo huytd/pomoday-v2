c494cea5f9827f79a698ccdbbf545a091fdb7d07
Huy Tran <kingbazoka@gmail.com>
2019-11-29 15:40:54 -0800

    No purge, pls

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,5 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: [
-    './src/**/*.html',
-    './src/**/*.tsx',
-    'index.html',
-  ],
-  whitelist: [
-    'h1',
-    'h2',
-    'h3',
-    'h4',
-    'h5',
-    'h6',
-    'ul',
-    'ol',
-    'li',
-    'p',
-    'pre',
-    'blockquote',
-    'figure',
-    'hr',
-    'table',
-    'img',
-    'div',
-    'span'
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
   plugins: [
-    require('tailwindcss')('./tailwind.config.js'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    require('tailwindcss')('./tailwind.config.js')
   ],
 };
